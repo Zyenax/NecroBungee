07c5e1707da70814793248101a632dff2c2b59a2
Zyenax <Zyenax@gmail.com>
2017-06-04 03:05:18 -0700

    Add files via upload

--- NecroBungee/src/main/java/net/necro/main/events/PlayerJoin.java
+++ NecroBungee/src/main/java/net/necro/main/events/PlayerJoin.java
@@ -1,13 +1,9 @@
 package net.necro.main.events;
 
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.necro.main.Necro;
-import net.necro.main.SQL.Rank;
-import net.necro.main.SQL.SQLRanks;
 import net.necro.main.utils.Utils;
 
 public class PlayerJoin implements Listener{
@@ -26,16 +22,6 @@ public class PlayerJoin implements Listener{
 			e.setCancelled(true);
 			e.getConnection().disconnect(Utils.color("&c&lWARNING \n &cERROR: &eDisconnected \n &cREASON: &eInvalid Version \n &cFIX: &eUpdate Client \n &cYOUR VERSION: &e" + e.getConnection().getVersion() + "\n &cSUPPORTED VERSIONS: &e" + Necro.verNum + " and up"));
 		}
-	}
-	
-	@EventHandler
-	public void onJoin(PostLoginEvent e){
-		final ProxiedPlayer p = (ProxiedPlayer) e.getPlayer();
-		if(SQLRanks.getRank(p) == null){
-			SQLRanks.setRank(p, Rank.DEFAULT.getName());
-		}
-		SQLRanks.loadProxiedPlayer(p);
-		
 	}
 
 }
